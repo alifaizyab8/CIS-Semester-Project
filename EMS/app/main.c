3dd0a6560f7e959087dab28993f15a65698881d8
alifaizyab8 <alifaizyab8@gmail.com>
2025-10-21 22:56:23 +0500

    Initialized Count and Array

--- EMS/app/main.c
+++ EMS/app/main.c
@@ -6,6 +6,14 @@
 
 int main()
 {
+    // Employee array
+    struct Employee employees[MAX_EMPLOYEES];
+    // Where ever *count is used, it is to keep track of the number of employees currently in the system
+    // we pass employeeCount with & to send its address
+    int employeeCount = 0;
+    
+    
+    
     displayLoginScreen();
     return 0;
 }
