d40eaba2e13fc84bb28d9f1a1a4f840eaf149ca6
alifaizyab8 <alifaizyab8@gmail.com>
2025-11-08 14:23:40 +0500

    Main Funciton Cleared, Initial print Menu created

--- EMS/app/extras.c
+++ EMS/app/extras.c
@@ -1,37 +1,67 @@
 #include "../include/extras.h"
 #include "../include/employee.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-
-void showMenu(struct Employee employees[], int *employeeCount) {
+void showMenu(struct Employee employees[], int *employeeCount)
+{
     int choice;
+    int id;
+    while (1)
+    {
+        
+        printf("\n\033[1;36m=====================================\033[0m\n");
+        printf("\033[1;33m        EMPLOYEE MANAGEMENT MENU      \033[0m\n");
+        printf("\033[1;36m=====================================\033[0m\n");
+        printf("1. Add New Employee\n");
+        printf("2. Remove Employee\n");
+        printf("3. Exit\n");
+        printf("\033[1;36m-------------------------------------\033[0m\n");
+        printf("Enter your choice (1-3): ");
 
-    while (1) {
-        printf("\n====== MENU ======\n");
-        printf("1. DISPLAY SINGLE EMPLOYEE\n");
-        printf("2. ADD EMPLOYEE\n");
-        printf("3. REMOVE EMPLOYEE\n");
-        printf("4. EXIT\n");
-        printf("ENTER YOUR DESIRED OPTION: ");
-        scanf("%d", &choice);  
-        switch (choice) {
-            case 1:
-                printf("DISPLAYING SINGLE EMPLOYEE:\n");
-                displaySingleEmployee(employees, *employeeCount, employees[0].id);
-                break;
-            case 2:
-                printf("ADDING NEW EMPLOYEE:\n");
-                addEmployee(employees, employeeCount);
-                break;
-            case 3:
-                printf("REMOVING EMPLOYEE:\n");
-                removeEmployee(employees, employeeCount, employees[0].id);
-                break;
-            case 4:
-                printf("EXITING PROGRAM\n");
-                return;
-            default:
-                printf("INVALID SELECTION!\n");
+        if (scanf("%d", &choice) != 1)
+        {
+            printf("\033[1;31mInvalid input! Please enter a number.\033[0m\n");
+            while (getchar() != '\n');
+            continue;
         }
+
+        switch (choice)
+        {
+        case 1:
+            printf("\n\033[1;34m--- ADDING NEW EMPLOYEE ---\033[0m\n");
+            if (addEmployee(employees, employeeCount))
+                printf("\033[1;32mEmployee added successfully!\033[0m\n");
+            else
+                printf("\033[1;31mFailed to add employee.\033[0m\n");
+            break;
+
+        case 2:
+            printf("\n\033[1;34m--- REMOVING EMPLOYEE ---\033[0m\n");
+            printf("Enter Employee ID to remove: ");
+            if (scanf("%d", &id) != 1)
+            {
+                printf("\033[1;31mInvalid ID input!\033[0m\n");
+                while (getchar() != '\n');
+                break;
+            }
+            if (removeEmployee(employees, employeeCount, id))
+                printf("\033[1;32mEmployee removed successfully!\033[0m\n");
+            else
+                printf("\033[1;31mEmployee not found or removal failed.\033[0m\n");
+            break;
+
+        case 3:
+            printf("\n\033[1;35mExiting program... Goodbye!\033[0m\n");
+            return;
+
+        default:
+            printf("\033[1;31mInvalid choice! Please select between 133.\033[0m\n");
+        }
+
+        printf("\nPress Enter to return to menu...");
+        while (getchar() != '\n');
+        getchar();
     }
 }
+
--- EMS/app/main.c
+++ EMS/app/main.c
@@ -1,38 +1,18 @@
 #include <stdio.h>
+#include"../include/extras.h"
 #include "../include/login.h"
 #include "../include/employee.h"
-
-// RUN COMMAND FOR NOW : gcc main.c employee.c login.c && ./a.exe
 int main()
 {
-    // Display Login Screen
-    //displayLoginScreen();
-    // Employee array
+
+    //Very Important Lines below
     struct Employee employees[MAX_EMPLOYEES];
-    
-    /*
-    My load employee function returns the number of employees loaded and loads
-    data from employee.txt into the employees array
-    */
     int employeeCount = loadEmployee(employees, MAX_EMPLOYEES);
-    // Initialize employee records at the start of the program
+
+
+    showMenu(employees,&employeeCount);
+
     
 
-    // Testing Add Employee Function
-    int temp = addEmployee(employees, &employeeCount);
-    if (temp)
-        printf("Employee added successfully. Total Employees: %d\n", employeeCount);
-    else
-        printf("Failed to add employee.\n");
-    displaySingleEmployee(employees, MAX_EMPLOYEES, employees[10].id);
-    int temp2 = removeEmployee(employees, &employeeCount, employees[9].id);
-    if (temp2)
-        printf("Employee removed successfully. Total Employees: %d\n", employeeCount);
-    else
-        printf("Failed to remove employee.\n");
-    displaySingleEmployee(employees, MAX_EMPLOYEES, employees[9].id);
-
-
- void showMenu();
     return 0;
 }
