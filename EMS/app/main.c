513c1ab591ceb40eb0b2feb72dde068d8db0f308
alifaizyab8 <alifaizyab8@gmail.com>
2025-11-08 15:03:41 +0500

    Added Run Command Comment

--- EMS/app/main.c
+++ EMS/app/main.c
@@ -14,3 +14,11 @@ int main()
 
     return 0;
 }
+/*
+To run the program, compile all the .c files together and execute the resulting binary.
+Example compilation command using gcc:
+        >>>gcc main.c employee.c extras.c login.c search.c
+Then run the program:
+        >>>./a.exe
+
+*/
