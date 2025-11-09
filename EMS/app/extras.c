33661c2e265386cbfca933ada430cc15589a468c
AnsharaAmir <ansharaamir0@gmail.com>
2025-11-09 23:50:35 +0500

    Fixed alignment in display all employees function

--- EMS/app/extras.c
+++ EMS/app/extras.c
@@ -138,13 +138,13 @@ void displayAllEmployees(const struct Employee employees[], int employeeCount)
     line();
     printf("\n\033[1;34mAll Employees List\033[0m\n");
 
-    printf("\033[1;33m%-5s %-15s %-15s %-5s %-15s %-10s %-12s %-10s %-10s\033[0m\n",
+    printf("\033[1;33m%-5s %-15s %-15s %-5s %-20s %-20s %-20s %-10s %-10s\033[0m\n",
            "ID", "First Name", "Last Name", "Age", "Position", "Salary", "Working Hrs", "Overtime", "Rating");
     line();
 
     for (int i = 0; i < employeeCount; i++)
     {
-        printf("%-5d %-15s %-15s %-5d %-15s %-10.2f %-12d %-10d %-10.2f\n",
+        printf("%-5d %-15s %-15s %-5d %-20s %-10.2f %-20d %-10d %-10.2f\n",
                employees[i].id,
                employees[i].emp.firstname,
                employees[i].emp.lastname,
